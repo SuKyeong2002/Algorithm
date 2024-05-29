ad872affa4165e2c933974c7f98355d49083b010
Sukyeong <158766114+SuKyeong2002@users.noreply.github.com>
2024-05-30 00:11:53 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] agrs) {
+        Scanner sc = new Scanner(System.in);
+        
+        String id = sc.next();
+        
+        System.out.print(id + "??!");
+        sc.close();
+    }
+}
