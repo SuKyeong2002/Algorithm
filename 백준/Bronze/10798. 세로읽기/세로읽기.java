b9ffac3a485c9ed9ba66671a825f23d13a8f0563
Sukyeong <158766114+SuKyeong2002@users.noreply.github.com>
2024-08-27 23:34:44 +0900

    [Bronze I] Title: 세로읽기, Time: 176 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] words = new String[5];
+
+        for (int i = 0; i < 5; i++) {
+            words[i] = scanner.nextLine();
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i < words[j].length()) {
+                    result.append(words[j].charAt(i));
+                }
+            }
+        }
+        
+        System.out.println(result.toString());
+    }
+}
