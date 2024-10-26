2d86fc0509911bdaf2387e1d5d1b7f7360a483c3
Sukyeong <158766114+SuKyeong2002@users.noreply.github.com>
2024-10-26 23:55:24 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 172 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -1,18 +1,24 @@
 import java.util.Scanner;
- 
+
 public class Main {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
-		int x = in.nextInt();
-		int y = in.nextInt();
-		int w = in.nextInt();
-		int h = in.nextInt();
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int w = scanner.nextInt();
+        int h = scanner.nextInt();
         
-		int x_min = Math.min(x, w-x);	// x축 최소거리
-		int y_min = Math.min(y, h-y);	// y축 최소거리
+        int distanceToLeft = x;          
+        int distanceToRight = w - x;     
+        int distanceToBottom = y;        
+        int distanceToTop = h - y;       
         
-		// x와 y축 최소거리 중 가장 작은 값
-		System.out.println(Math.min(x_min, y_min));
-	}
+        int minDistance = Math.min(Math.min(distanceToLeft, distanceToRight),
+                                   Math.min(distanceToBottom, distanceToTop));
+        
+        System.out.println(minDistance);
+        
+        scanner.close();
+    }
 }
