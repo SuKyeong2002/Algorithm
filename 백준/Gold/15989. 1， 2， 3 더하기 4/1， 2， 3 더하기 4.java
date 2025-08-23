8f32699e079a00a7df2a208fb20b15ee64f8e795
오수경 <osukyeong0109@gmail.com>
2025-08-23 20:28:25 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 112 ms, Memory: 14676 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 테스트 케이스의 개수 입력받기
+        int testCase = Integer.parseInt(br.readLine());
+
+        // 테스트 케이스의 정수를 저장하는 배열 생성
+        int[] TestCaseArr = new int[testCase];
+
+        // 테스트 케이스 배열 값 입력받기
+        int max = 0;
+        for (int i = 0; i < testCase; i++) {
+            TestCaseArr[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, TestCaseArr[i]);
+        }
+
+        /*
+         * 배열값이 1, 2, 3으로 나눌 수 있는 방법의 수를 저장하는 배열 생성
+         * max + 1: 배열 값의 범위 + 1 -> > [0, 1, ..., max]
+         * 4: 나눠질 수 있는 경우의 수 + 1 -> [0, 1, 2, 3]
+         */
+        int[][] dp = new int[max + 1][4];
+
+        /*
+         * 배열값이 1, 2, 일 때 dp 초기값 설정
+         * dp[2][1] -> (1, 1)
+         * dp[2][2] -> (2)
+         * dp[3][1] -> (1, 1, 1)
+         * dp[3][2] -> (2, 1)
+         * dp[3][3] -> (3)
+         * *** 조건절이 등호가 아닌 부호인 이유: 큰 경우에서도 사용해야 하기 때문
+         */
+        dp[1][1] = 1;
+        if (max >= 2) {
+            dp[2][1] = dp[2][2] = 1;
+        }
+        if (max >= 3) {
+            dp[3][1] = dp[3][2] = dp[3][3] = 1;
+        }
+
+        /*
+         * 접근 방식: if (n / 2 > 0) → 다시 (n-2)를 1,2,3으로 나눌 수 있는지 재귀적으로 확인
+         * -> 문제점 1. 방법의 수 찾기 가능하나, 중복도 카운트
+         * -> 예. (2+1+1), (1+1+2), (1+2+1) -> 1인데 3으로 카운트
+         * -> 문제점 2. 시간 초과
+         * 해결방안. 순서 없는 조합 만들기 -> 누적합 계산하는 dp 사용
+         * 배열값이 4부터 max까지 반복
+         */
+        for (int i = 4; i <= max; i++) {
+            dp[i][1] = dp[i - 1][1];
+            dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
+            dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
+        }
+
+        // 결과 (각각의 테스트 케이스의 방법의 수의 합) 출력
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < testCase; i++) {
+            sb.append(dp[TestCaseArr[i]][1] + dp[TestCaseArr[i]][2] + dp[TestCaseArr[i]][3]).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
